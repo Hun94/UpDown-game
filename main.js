a5ef2337f41fa487309263784f759f05e88fc109
Jaehun Jeong <hpersona9452@gmail.com>
2022-07-25 08:09:10 +0900

    commit 3

--- main.js
+++ main.js
@@ -5,12 +5,22 @@
 // 실패 or 성공
 
 let random_number;
-let remaining_attempt;
+let game_over=false;
+let user_chance = 0;
 
-let check = document.getElementById("check-button")
-check.addEventListener("click", play);
+let start = document.getElementById("start-button");
+start.addEventListener("click", Start);
 
-let submit = document.getElementById("submit-number") 
+let check = document.getElementById("check-button");
+check.addEventListener("click", Play);
+
+let reset = document.getElementById("reset-button");
+reset.addEventListener("click", Reset)
+
+let submit = document.getElementById("submit-number"); 
+let chance = document.getElementById("chance-number");
+let result = document.getElementById("result-area"); 
+let remain = document.getElementById("remain-chance")
 
 
 function RN_maker () {
@@ -18,20 +28,44 @@ function RN_maker () {
     console.log("정답"+ random_number);    
 }
 
-function play(){
+function Start(){
+    RN_maker();
+    result.textContent = "시작"
+    user_chance=chance.value;
+    remain.textContent = chance.value;
+}
+
+function Play(){
     let user_input = submit.value;
-    
+
+    user_chance -- ;
+    remain.textContent = user_chance;
+
     if(user_input>random_number){
-        console.log("Down");
+        result.textContent = "Down"
     }
     else if(user_input<random_number){
-        console.log("Up");
+        result.textContent = "Up"
     }
     else{
-        console.log("정답입니다.");
+        result.textContent = "Correct"
+    }
+
+    if(user_chance<1){
+        game_over = true;
+        result.textContent = "Game Over"
+    }
+
+    if(game_over==true){
+        check.disabled = true
     }
 
 }
-
-RN_maker();
-
+function Reset(){
+    submit.value="";
+    chance.value="";
+    user_chance.value="";
+    RN_maker();
+    result.textContent = "Up or Down"
+    remain.textContent = "Remain chance"
+}
